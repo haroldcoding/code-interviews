7db765f79decb4d6e92edac1d6813ded863a97f8
haroldcoding <contactiharold@gmail.com>
2017-07-09 00:27:53 +0800

    添加 : 给定中序遍历和后序遍历结果重建二叉树解法

--- src/main/java/Number6.java
+++ src/main/java/Number6.java
@@ -15,8 +15,7 @@ public class Number6 {
             || pre.length != in.length) {
             return null;
         }
-        TreeNode rootNode = reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
-        return rootNode;
+        return reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
     }
     
     private TreeNode reConstructBinaryTree(int[] pre, int startPre, int endPre,
@@ -26,7 +25,7 @@ public class Number6 {
         }
         TreeNode root = new TreeNode(pre[startPre]);
         for (int i = startIn; i <= endIn; i++) {
-            if (pre[startIn] == in[i]) {
+            if (pre[startPre] == in[i]) {
                 root.left = reConstructBinaryTree(pre, startPre + 1, startPre + i - startIn,
                     in, startIn, i - 1);
                 root.right = reConstructBinaryTree(pre, startPre + i - startIn + 1, endPre,
@@ -52,4 +51,27 @@ public class Number6 {
         }
         return root;
     }
+    
+    /**
+     * 如果给定中序遍历和后序遍历的结果，重建二叉树。
+     * 分析思路同上，注意后序遍历根节点是序列最后一个值，如果根据中序遍历序列结合根节点值，判断出左子树节点有 m 个，
+     * 右子树节点有 n 个，那么后序遍历序列中根节点左侧 n 个数为右子树的节点，剩余的为左子树节点。
+     */
+    public TreeNode reConstructBinaryTree2(int[] in, int[] back) {
+        if (in == null || back == null || in.length == 0 || back.length == 0
+            || in.length != back.length) {
+            return null;
+        }
+        TreeNode root = new TreeNode(back[0]);
+        for (int i = 0, length = in.length; i < length; i++) {
+            if (in[0] == back[i]) {
+                root.left = reConstructBinaryTree(Arrays.copyOfRange(in, 0, i),
+                    Arrays.copyOfRange(back, 0, i));
+                root.right = reConstructBinaryTree(Arrays.copyOfRange(in, i + 1, in.length),
+                    Arrays.copyOfRange(back, i, back.length - 1));
+            }
+        }
+        return root;
+    }
+    
 }
